7ccf09f0f33eb0f95b6a6451f8289ea02f22ec5b
lechance <lechance@sina.com>
2020-08-18 08:43:06 +0800

    add some comments

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 set -o pipefail
-
 set +e
+: "${WORK_HOME:="/root/"}"
 
+# As argument is not main programs, assume user want to run his own process, for example a `bash` shell to explore this image
 exec "$@"
